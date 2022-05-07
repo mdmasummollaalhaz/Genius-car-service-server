3f3740c1261883b1b52147b77651ed558d2ec4ff
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-07 23:08:00 +0600

    connect server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 6001;
 
@@ -9,9 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('Running Genius Server');ca
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bi2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Genius car DB connected');
+  console.log('hello');
+  // perform actions on the collection object
+  client.close();
 });
+
+app.get('/', (req, res) =>{
+    res.send('Running Genius Server');
+});
+
+// hello upa
 
 app.listen(port, () =>{
     console.log('Listening to port', port);
