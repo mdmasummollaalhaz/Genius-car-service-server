6d656b33c622d7fb5943677791067dc56b8e4a0b
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-16 01:47:35 +0600

    heroku step

---
+++ __server_deploy.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * ------------
+ * ONE TIME
+ * ------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku CLI
+ * 4. Heroku Login
+ * -------------------
+ * 
+ * For each project
+ * ----------------
+ * 1. heroku create
+ */
