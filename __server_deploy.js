bd7425b9da31d47a46232605741cf5fe4237cd43
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-16 23:43:45 +0600

    commit

--- __server_deploy.js
+++ __server_deploy.js
@@ -7,9 +7,32 @@
  * 2. verify email
  * 3. install heroku CLI
  * 4. Heroku Login
- * -------------------
  * 
+ * 
+ * ------------------------------
  * For each project
- * ----------------
+ * ------------------------------
  * 1. heroku create
+ * 2. make sure: git add . & git commit -m'' & git push
+ * 3. git push heroku main
+ * 4. Gpp tp Heroku Dashboard > Current Project > settings > Reveal config
+ * 5. copy-paste config vars from your .env file
+ * 6. Make sure you have whitelisted all IP Address to access mongoDB
+ * 
+ * 
+ * ------------------------------
+ * UPDATE SERVER with new changes
+ * ------------------------------
+ * 1. Make changes
+ * 2. make sure: git add . & git commit -m'' & git push
+ * 3. git push heroku main
+ * 
+ * 
+ * ------------------------------
+ * Connect server with client and deploy client
+ * ------------------------------
+ * 1. replace localhost by heroku link
+ * 2. npm run build
+ * 3. firebase deploy
+ * 
  */
